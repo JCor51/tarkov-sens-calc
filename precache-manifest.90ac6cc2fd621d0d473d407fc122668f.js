a95d4debbed560eb5d87cd599a7fc503f286af7b
Jessy Coraboeuf <coraboeufjessy@gmail.com>
2020-03-02 20:31:04 +0100

    Updates

---
+++ precache-manifest.90ac6cc2fd621d0d473d407fc122668f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ba46d12dbb79f8d7831174fc433a8e7",
+    "url": "/tarkov-sens-calc/index.html"
+  },
+  {
+    "revision": "7f2027c439cac9868c2b",
+    "url": "/tarkov-sens-calc/static/css/main.3ffc20ac.chunk.css"
+  },
+  {
+    "revision": "8f9138648023c98f231a",
+    "url": "/tarkov-sens-calc/static/js/2.bbf28f45.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/tarkov-sens-calc/static/js/2.bbf28f45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f2027c439cac9868c2b",
+    "url": "/tarkov-sens-calc/static/js/main.261d12c2.chunk.js"
+  },
+  {
+    "revision": "c29c6f8c373f36658048",
+    "url": "/tarkov-sens-calc/static/js/runtime-main.5a6c9ee6.js"
+  },
+  {
+    "revision": "c2bed103e480ce62d945b4e65478a2a8",
+    "url": "/tarkov-sens-calc/static/media/eftlogo.c2bed103.png"
+  }
+]);
